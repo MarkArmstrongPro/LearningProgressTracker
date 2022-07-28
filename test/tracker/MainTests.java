b9acb487d48256d8c787ceaa23c04732a1117bc3
MarkArmstrongPro <MarkArmstrongProf@gmail.com>
2022-07-28 15:59:56 +0300

    chng: adds tests

--- test/tracker/MainTests.java
+++ test/tracker/MainTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class MainTests {
     @Test
@@ -18,7 +20,7 @@ public class MainTests {
         assertFalse(Main.isValidEmail("somegmail.com"));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index} : isValidEmail({1}) => {2}")
     @CsvSource({
             "some@gmail.com, true",
             "SoMe123@gmail.com, true",
@@ -26,5 +28,18 @@ public class MainTests {
     })
     void testIsValidEmailParameterized(String email, boolean expected) {
         assertEquals(expected, Main.isValidEmail(email));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"some@mail.com", "another@email.org"})
+    void testIsValidEmailParameterizedValues(String arg){
+        assertTrue(Main.isValidEmail(arg));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = { " ", "   ", "\t", "\n" })
+    void testIsValidEmailNullAndEmpty(String arg) {
+        assertTrue(arg == null || arg.trim().isEmpty());
     }
 }
